afbf107f554c8c41d481f16884816416699e43d0
Scott R Bowe <scottb@sentania.net>
2025-12-09 13:24:44 -0600

    try the new module again

--- deployments.tf
+++ deployments.tf
@@ -1,16 +1,20 @@
-#Catalog Item request
-
 module "deployments" {
-  source   = "sentania-labs/vmapps-consume-catalogitem/vra"
-  version  = "0.5.0"
+  source   = "sentania-labs/vmapps-deployment/vra"
+  version  = "0.3.0"
   for_each = var.deployments
 
-  deployment_name      = each.value.deployment_name
-  project_name         = var.project_name
-  description          = each.value.description
-  catalog_item_name    = each.value.catalog_item_name
-  catalog_item_version = each.value.catalog_item_version
-  inputs               = each.value.inputs
+  project_name    = var.project_name
+  deployment_name = each.value.deployment_name
+  description     = each.value.description
+  inputs          = each.value.inputs
+
+  # Blueprint path
+  blueprint_name    = try(each.value.blueprint_name, null)
+  blueprint_version = try(each.value.blueprint_version, null)
+
+  # Catalog item path
+  catalog_item_name    = try(each.value.catalog_item_name, null)
+  catalog_item_version = try(each.value.catalog_item_version, null)
 }
 
 data "vra_machine" "all" {
